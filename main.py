7139253394b62ef4a1454ab466b1b2feba4d60c3
sirapol <s.sirapol@hotmail.com>
2020-09-03 14:31:09 +0700

    interval update

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import json
 import requests
 import random
+import time
+
+interval = 5
 
 host = "yourHost"
 port = 8200
 token = "yourToken"
-header = {"Content-Tpye":"application/json"}
-
-strdata = {"data0":str(random.randint(0,100))}
+header = {"Content-Tpye": "application/json"}
 
 url = "http://" + host + ":" + str(port) + "/api/v1/" + token + "/telemetry"
+print("send to :", url)
+try:
+    while True:
+        strdata = {"data0": str(random.randint(0, 100))}
 
-print("send to :",url)
-jsonData = json.dumps(strdata)
-print(jsonData)
-response = requests.post(url,data=jsonData,headers=header)
-print(response)
+        jsonData = json.dumps(strdata)
+        print(jsonData)
+        response = requests.post(url, data=jsonData, headers=header)
+        print(response)
+        time.sleep(interval)
+except KeyboardInterrupt:
+    pass
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # README.md
 
 This python script is send data to ThingsBoard via REST API.
+It will update data every 5sec. (Interval able to change)
